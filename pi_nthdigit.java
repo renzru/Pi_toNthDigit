45823308facc3d2bf2570c31350ac3d1bb6bf47e
renzru <112093726+renzru@users.noreply.github.com>
2022-09-08 11:27:28 +0800

    Update pi_nthdigit.java

--- pi_nthdigit.java
+++ pi_nthdigit.java
@@ -3,26 +3,30 @@ import java.lang.Math;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+// TODO: make it so that it can print more decimals of pi than the max 15 digits in Math.PI (aybe use a formula for generating pi?)
+
 public class pi_nthdigit{
     public static void main(String[]args)
     {
         Scanner scan = new Scanner(System.in);
-        System.out.print("PI to how many decimal places?: ");
+        System.out.print("Pi to how many decimal places?: ");
         int num = scan.nextInt();
-    
-        System.out.println(round(Math.PI, num));
-    }
-
-    public static BigDecimal round(double pi, int place)
-    {
         int max = 15; // max length of Math.PI
         
-        if (place <= 0 || place > max)
+        if (num > 0 && num <= max)
+        {
+            System.out.println(round(num));
+        }
+
+        else 
         {
             System.out.println("Invalid!");
-            System.exit(0);
         }
-            BigDecimal bd = new BigDecimal(Double.toString(pi));
+    }
+
+    public static BigDecimal round(int place)
+    {
+            BigDecimal bd = new BigDecimal(Double.toString(Math.PI));
             bd = bd.setScale(place, RoundingMode.DOWN);
             return bd;
     }
