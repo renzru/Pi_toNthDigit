4bd4887e159368b5fc3fbdaa68e4f43614badf65
renzru <112093726+renzru@users.noreply.github.com>
2022-09-08 09:25:51 +0800

    Add files via upload

---
+++ pi_nthdigit.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class pi_nthdigit{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("PI to how many decimal places?: ");
+        int num = scan.nextInt();
+    
+        System.out.println(roundup(Math.PI, num));
+    }
+
+    public static BigDecimal roundup(double pi, int place)  // method for rounding up
+    {
+        int max = 15; // max length of Math.PI
+        
+        if (place <= 0 || place > max)
+        {
+            System.out.println("Invalid!");
+            System.exit(0);
+        }
+            BigDecimal bd = new BigDecimal(Double.toString(pi));
+            bd = bd.setScale(place, RoundingMode.DOWN);
+
+            return bd;
+    }
+}
